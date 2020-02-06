0049871154ef1423e603271d5292bed12ce8d4c4
Spencer Elliott <s.elliott123@gmail.com>
2020-02-05 22:09:41 -0500

    Fixed PPM output

--- src/main.rs
+++ src/main.rs
@@ -24,23 +24,12 @@ fn main() -> std::io::Result<()> {
     p3_file.write_fmt(format_args!("P3\n{} {}\n255\n", 64, 32))?;
 
     for byte in test_system.get_framebuffer() {
-        let colors = [
-            if byte & 0b1000_0000 > 0 { 255 } else { 0 },
-            if byte & 0b0100_0000 > 0 { 255 } else { 0 },
-            if byte & 0b0010_0000 > 0 { 255 } else { 0 },
-            if byte & 0b0001_0000 > 0 { 255 } else { 0 },
-            if byte & 0b0000_1000 > 0 { 255 } else { 0 },
-            if byte & 0b0000_0100 > 0 { 255 } else { 0 },
-            if byte & 0b0000_0010 > 0 { 255 } else { 0 },
-            if byte & 0b0000_0001 > 0 { 255 } else { 0 },
-        ];
+        let color = if byte > &0 { 255 } else { 0 };
 
-        for color in colors.iter() {
-            p3_file.write_fmt(format_args!(
-                "{0} {0} {0}\n",
-                color
-            ))?;
-        }
+        p3_file.write_fmt(format_args!(
+            "{0} {0} {0}\n",
+            color
+        ))?;
     }
 
     println!("Hello, world!");
